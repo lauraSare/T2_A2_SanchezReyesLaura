0b42a76544e5260d7b51a2fc08a6fbe02e8569cb
Laura Sanchez <laura.sr@tecjerez.edu.mx>
2026-03-17 19:13:52 -0600

    conexion clever cloud

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const mysql = require('mysql2');
 
@@ -19,3 +19,23 @@ conexion.connect( function(err){
 module.exports = conexion;
 
 
+*/
+'use strict';
+
+const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+    host: 'b4nqdcelzbjfezqnlfun-mysql.services.clever-cloud.com', 
+    user: 'ufwz3lvsbfytcppw', 
+    password: '408lAAQxnbEnjLApp335', 
+    database: 'b4nqdcelzbjfezqnlfun',
+    port: 3306
+});
+
+conexion.connect(function(err){
+    if(err)
+        throw err;
+    console.log('Conexión BD Exitosa!!')
+});
+
+module.exports = conexion;
